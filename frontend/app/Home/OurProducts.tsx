05b08ea18ffaac049f223c384580ccc8963de9cb
Ali <ali.shiekh16@outlook.com>
2025-03-23 01:30:07 +0500

    Add our products section.

---
+++ frontend/app/Home/OurProducts.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+import React from 'react';
+import ButtonOutline from '../components/ButtonOutline';
+
+const OurProducts = () => {
+  return (
+    <div className='h-fit'>
+      <h2 className='bg-primary-darker uppercase text-7xl font-bold text-center py-2 text-white'>
+        Our Products
+      </h2>
+      <div className='grid grid-cols-2  max-h-max'>
+        <div className='flex flex-col'>
+          <Image
+            src='/images/home/skin-model.png'
+            alt='serum'
+            width={500}
+            height={500}
+            className='object-cover w-full h-full translate-y-2'
+          />
+          <div className='px-10 py-20 space-y-2 h-fit '>
+            <h3 className='text-4xl'>Skin Care Products</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Augue in ornare accumsan
+              ultricies enim lacus nunc vestibulum. Gravida vitae sed porttitor
+              condimentum. Ullamcorper auctor tincidunt amet aliquam tellus. Mi
+              odio ullamcorper egestas facilisi in pellentesque pellentesque
+              lacus lectus.
+            </p>
+            <ButtonOutline className=' text-md py-6 font-semibold mt-5'>
+              Shop Now
+            </ButtonOutline>
+          </div>
+        </div>
+
+        <div className='flex flex-col'>
+          <Image
+            src='/images/home/hair-model.png'
+            alt='serum'
+            width={500}
+            height={500}
+            className='object-cover w-full h-full'
+          />
+          <div className='bg-primary-variant text-white px-10 py-20 space-y-2 h-fit'>
+            <h3 className='text-4xl'>Hair Care & Treatments</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Augue in ornare accumsan
+              ultricies enim lacus nunc vestibulum. Gravida vitae sed porttitor
+              condimentum. Ullamcorper auctor tincidunt amet aliquam tellus. Mi
+              odio ullamcorper egestas facilisi in pellentesque pellentesque
+              lacus lectus.
+            </p>
+            <ButtonOutline className='border-white bg-transparent text-md py-6 font-semibold mt-5'>
+              Shop Now
+            </ButtonOutline>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurProducts;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeCover from './Home/HomeCover';
 import SectionIntro from './Home/SectionIntro';
+import OurProducts from './Home/OurProducts';
 
 const Page = () => {
   return (
     <>
       <HomeCover />
       <SectionIntro />
+      <OurProducts />
     </>
   );
 };
