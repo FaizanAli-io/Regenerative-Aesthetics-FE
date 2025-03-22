ae9e3a012ce94bf9b29e3826e19ac162fd35b7ae
Ali <ali.shiekh16@outlook.com>
2025-03-23 00:48:07 +0500

    Add intro section.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeCover from './Home/HomeCover';
+import SectionIntro from './Home/SectionIntro';
 
 const Page = () => {
   return (
     <>
       <HomeCover />
+      <SectionIntro />
     </>
   );
 };
