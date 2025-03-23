1b14b704bd08882334abeeae5b90061c6578af77
Ali <ali.shiekh16@outlook.com>
2025-03-23 19:39:27 +0500

    Add Recommended Product section.

--- frontend/app/product-details/page.tsx
+++ frontend/app/product-details/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductPrimaryInfo from './_sections/ProductPrimaryInfo';
+import RecommendedProducts from './_sections/RecommendedProducts';
 
 const Page = () => {
   return (
     <>
       <ProductPrimaryInfo />
+      <RecommendedProducts />
     </>
   );
 };
