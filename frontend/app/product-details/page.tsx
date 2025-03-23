d5694ed04bfe1dfa504e87c5f3cc493b8595a96b
Ali <ali.shiekh16@outlook.com>
2025-03-24 00:29:43 +0500

    Add related products section.

--- frontend/app/product-details/page.tsx
+++ frontend/app/product-details/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductPrimaryInfo from './_sections/ProductPrimaryInfo';
 import RecommendedProducts from './_sections/RecommendedProducts';
 import ReviewSummary from './_sections/ReviewSummary';
+import RelatedProducts from './_sections/RelatedProducts';
 
 const Page = () => {
   return (
@@ -9,6 +10,7 @@ const Page = () => {
       <ProductPrimaryInfo />
       <RecommendedProducts />
       <ReviewSummary />
+      <RelatedProducts />
     </>
   );
 };
