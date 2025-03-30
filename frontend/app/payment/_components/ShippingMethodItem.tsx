a99164cbd446784c283e0364fec2722ad3ec5990
Ali <ali.shiekh16@outlook.com>
2025-03-30 17:55:55 +0500

    Add payment method section.

---
+++ frontend/app/payment/_components/ShippingMethodItem.tsx
@@ -0,0 +1,46 @@
+import { Checkbox } from '@/components/ui/checkbox';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  charges: string;
+  text: string;
+  date: string;
+}
+
+const ShippingMethodItem = ({
+  charges,
+  text,
+  date,
+  className,
+  ...props
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        'border-1 border-gray-400 flex items-center justify-between space-x-5 p-5 text-2xl text-primary-darker rounded-md',
+        className
+      )}
+      {...props}
+    >
+      <div className='flex items-center space-x-5 text-xl'>
+        <div className='flex items-center space-x-2'>
+          <Checkbox
+            className=' data-[state=checked]:bg-primary-variant2'
+            id='billing-address'
+          />
+          <label
+            htmlFor={text}
+            className='font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+          >
+            {charges}
+          </label>
+        </div>
+        <p>{text}</p>
+      </div>
+      <p className='font-semibold text-lg'>{date}</p>
+    </div>
+  );
+};
+
+export default ShippingMethodItem;
--- frontend/app/payment/page.tsx
+++ frontend/app/payment/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import OrderSummary from './OrderSummary';
 import CreditCardForm from './CreditCardForm';
+import ShippingMethod from './_sections/ShippingMethod';
 
 const Page = () => {
   return (
     <>
+      <ShippingMethod />
       <OrderSummary />
       <CreditCardForm />
     </>
