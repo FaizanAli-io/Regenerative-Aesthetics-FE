cb02776d979f3c0b7f61bc8cb5945ea4fb6574a9
Faizan Ali <fali.abdulali@gmail.com>
2025-04-16 17:03:28 +0500

    update data source

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -9,8 +9,8 @@ export const dataSourceOptions: DataSourceOptions =
   {
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    entities: ['dist/src/**/*.entity.js'],
-    migrations: ['dist/db/migrations/*.js'],
+    entities: ['../src/**/*.entity.js'],
+    migrations: ['../db/migrations/*.js'],
     logging: false,
     synchronize: false,
     ssl: { rejectUnauthorized: false },
