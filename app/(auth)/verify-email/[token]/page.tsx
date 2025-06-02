8c397c45061e92cb79769a71a00e7ce4e0a40c3d
Faizan Ali <fali.abdulali@gmail.com>
2025-06-02 19:12:24 +0500

    A little linking

--- app/(auth)/verify-email/[token]/page.tsx
+++ app/(auth)/verify-email/[token]/page.tsx
@@ -16,8 +16,6 @@ const page = async ({ params }: Props) => {
   } catch (ex) {
     if (axios.isAxiosError(ex)) {
       if (ex.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
         console.error("Error response:", ex.response.data);
       }
     }
@@ -29,7 +27,9 @@ const page = async ({ params }: Props) => {
       <h1 className="text-3xl font-bold">Verify Email</h1>
       <p className="mt-4 text-lg">Your email has been verified</p>
       <p className="mt-2 text-lg">You can now login to your account.</p>
-      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Go to login</button>
+      <a href="/auth" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded text-center">
+        Go to login
+      </a>
     </div>
   );
 };
