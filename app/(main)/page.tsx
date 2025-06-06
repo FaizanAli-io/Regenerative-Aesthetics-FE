d4e24c98c3ca7841dabed64b387ac6012a51d313
Ali <ali.shiekh16@outlook.com>
2025-06-06 13:20:43 +0500

    Remove blogs highlights.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,7 @@ import HomeCover from './Home/HomeCover';
 import SectionIntro from './Home/SectionIntro';
 import OurProducts from './Home/OurProducts';
 import SpecialOffer from './Home/SpecialOffer';
-import BlogHighlight from './Home/BlogHighlight';
+// import BlogHighlight from './Home/BlogHighlight';
 import NaturallyCrafted from './Home/NaturallyCrafted';
 import GetInTouch from './Home/GetInTouch';
 import Reviews from './Home/Reviews';
@@ -17,7 +17,7 @@ const Page = () => {
       <OurProducts />
       <SpecialOffer />
       <SectionAnimation />
-      <BlogHighlight />
+      {/* <BlogHighlight /> */}
       <NaturallyCrafted />
       <Reviews />
       <GetInTouch />
