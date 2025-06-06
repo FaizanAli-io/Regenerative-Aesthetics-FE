8a15dfb7f90da72b12200d6ba8b59b260b94ac8b
Ali <ali.shiekh16@outlook.com>
2025-06-06 17:39:33 +0500

    Add links on the home page.

--- app/(main)/Home/SectionAnimation.tsx
+++ app/(main)/Home/SectionAnimation.tsx
@@ -44,7 +44,7 @@ const SectionAnimation = () => {
   );
 
   return (
-    <div ref={container} className='relative w-full overflow-hidden h-[400px]'>
+    <div ref={container} className='relative w-full overflow-hidden h-screen'>
       <img
         src='/blob.png'
         className='w-full h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blob'
